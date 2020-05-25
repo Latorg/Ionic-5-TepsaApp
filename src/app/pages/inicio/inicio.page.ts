76b7b07869a171df17bb98c2ff0679278af33b3f
Abraham Galicia <torres.abraham@live.com>
2020-05-25 15:43:58 -0500

    Home page modified

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../../app.component';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
@@ -6,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  
+  features = [
+    {
+      url: '/assets/images/home/delivery.png',
+      text: 'Contamos con servicio de entrega a domicilio.'
+    },
+    {
+      url: '/assets/images/home/stock.png',
+      text: 'Contamos con gran inventario y tiempos de entrega ágiles.'
+    },
+    {
+      url: '/assets/images/home/lowprice.png',
+      text: 'Los precios más bajos en el mercado.'
+    },
+    {
+      url: '/assets/images/home/salesman.png',
+      text: 'Personal calificado para ayudar en tus compras.'
+    },
+  ];
 
-  constructor() { }
+  public isMobileResolution: boolean;
+  fontStyle: string;
+  
+  constructor() {
+    this.isMobileResolution = AppComponent.isMobileResolution;
+    this.fontStyle = this.isMobileResolution ? '' : 'xx-large';
+  }
   ngOnInit() {}
 
   
