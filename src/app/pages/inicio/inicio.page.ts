152c1efef22f5e022e6cd7581c1056d594e4d19f
Abraham Galicia <torres.abraham@live.com>
2020-06-19 11:29:25 -0500

    Home page modified, new features added

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -12,20 +12,34 @@ export class InicioPage implements OnInit {
 
   features = [
     {
-      url: '/assets/images/home/delivery.png',
+      url: '/assets/images/home/delivery green.png',
+      title: 'Servicio',
       text: 'Servicio de Entrega (aplican términos y condiciones)'
     },
     {
-      url: '/assets/images/home/stock.png',
+      url: '/assets/images/home/stock green.png',
+      title: 'Inventario',
       text: 'Contamos con un amplio surtido de artículos en existencia'
     },
     {
-      url: '/assets/images/home/lowprice.png',
+      url: '/assets/images/home/lowprice green.png',
+      title: 'Precios bajos',
       text: 'Los precios más bajos en el mercado'
     },
     {
-      url: '/assets/images/home/salesman.png',
-      text: 'Tienes dudas? Nuestro personal amable y calificado te ayudará'
+      url: '/assets/images/home/salesman green.png',
+      title: 'Asesoría',
+      text: '¿Tienes dudas? Nuestro personal amable y calificado te ayudará'
+    },
+    {
+      url: '/assets/images/home/experience green.png',
+      title: 'Experiencia',
+      text: 'Contamos con más de 35 años de experiencia en el mercado'
+    },
+    {
+      url: '/assets/images/home/garanty green.png',
+      title: 'Garantía',
+      text: 'Manejamos productos de la más alta calidad'
     },
   ];
 
