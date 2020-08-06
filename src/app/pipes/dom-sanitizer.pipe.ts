0072dbfd6e2aabce89c10522d3f4a5ad6c52a3d7
Abraham Galicia <torres.abraham@live.com>
2020-08-06 13:26:26 -0500

    Added new products and product description height now depends on description length

--- src/app/pipes/dom-sanitizer.pipe.ts
+++ src/app/pipes/dom-sanitizer.pipe.ts
@@ -7,7 +7,7 @@ export class DomSanitizerPipe implements PipeTransform {
 
   constructor( private domSan: DomSanitizer ) {}
 
-  transform( img: string, height: string): any {
+  transform( img: string, height: number): any {
     const domImg = `background-image: url('${ img }'); min-height: ${ height }px; max-height: ${ height }px`;
     return this.domSan.bypassSecurityTrustStyle( domImg );
   }
